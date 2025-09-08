2204aa42ecdb1b6958483edead4359fd508bbe11
surjeet <surjeet@gmail.com>
2025-09-08 23:06:50 +0530

    initial set up app

--- ras/ras/settings.py
+++ ras/ras/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myadmin',
+    'employee',
+    'home',
+    'accounts',
+
 ]
+
+LOGIN_REDIRECT_URL = 'home:landing'
+LOGOUT_REDIRECT_URL = 'home:landing'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,10 +83,15 @@ WSGI_APPLICATION = 'ras.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   
+        'NAME': 'rasdb',                           
+        'USER': 'admin',                              
+        'PASSWORD': 'admin',                          
+        'HOST': 'localhost',                        
+        'PORT': '5432',                             
     }
 }
+
 
 
 # Password validation
@@ -115,7 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
