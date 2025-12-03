00f6b47b2e2e88950b432cbaa107dd69c3afa614
Surjeet Kumar <104543098+8851962638@users.noreply.github.com>
2025-12-03 23:05:17 +0530

    Update settings.py

--- ras/settings.py
+++ ras/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
