d03428c035f22484953c95b0285a7f678fd5302d
Surjeet Kumar <104543098+8851962638@users.noreply.github.com>
2026-01-18 02:13:53 +0530

    Update settings.py

--- ras/settings.py
+++ ras/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'accounts.translation_debug.TranslationDebugMiddleware',
     'accounts.middleware.ProfileCompletionMiddleware',
 ]
 
